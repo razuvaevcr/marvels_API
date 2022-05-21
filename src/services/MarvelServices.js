6c83a4db221a9d86adaf38097f990769d1706d57
MyNameIs <razuvaj@gmail.com>
2022-05-21 21:42:43 +0300

    create an app and change its structure + add MarvelServices and set up some api requests

---
+++ src/services/MarvelServices.js
@@ -0,0 +1,35 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=8542f82929ec266d42369280bb12c92e';
+    getResourse = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url} status:${res.status}`);
+        }
+
+        return await res.json()
+    }
+
+    getAllCharacters = async() => {
+        const res = await this.getResourse(`${this._apiBase}/characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResourse(`${this._apiBase}/characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
